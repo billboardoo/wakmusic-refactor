98425cee52b411ba4900c982295012e069a6c0ce
songiwon <biob1121@naver.com>
2022-11-13 23:23:22 +0900

    feat: 페이지 소개 부분 컴포넌트 분할

---
+++ src/components/PageIntroduce.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PageIntroduce = () => {
+  return (
+    <>
+      <div className="page-title-wrap">
+        <div className="title-sub">WAKTAVERSE MUSIC</div>
+        <div className="title fadein">재생목록</div>
+      </div>
+      <div className="title-line" />
+    </>
+  );
+};
+
+export default PageIntroduce;
